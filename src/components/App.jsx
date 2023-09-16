bb2d166d7bf573ac44ed7926fe67d62a68b22546
Serhiy8 <markovsm86@gmail.com>
2023-09-16 17:21:14 +0300

    React-toastify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { fetchImagesIPA } from './Api/api';
@@ -11,7 +13,6 @@ function App() {
   const [page, setPage] = useState(1);
   const [totalPage, setTotalPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
 
   const loaderToggle = isLoading => {
     setLoading(isLoading);
@@ -23,14 +24,13 @@ function App() {
 
   const handleSubmit = valueFromInput => {
     if (!valueFromInput) {
-      setErrorMessage('Please enter a valid value');
+      toast.info('Please enter a valid value');
       setInputValue('');
       setImgList([]);
       setPage(1);
       setTotalPage(1);
     } else {
       setInputValue(valueFromInput);
-      setErrorMessage('');
       setPage(1);
       setTotalPage(1);
     }
@@ -53,13 +53,12 @@ function App() {
           setImgList(prevImgList =>
             page === 1 ? response.hits : [...prevImgList, ...response.hits]
           );
-          setErrorMessage('');
         } else {
-          setErrorMessage('Nothing found. Please try a different search term.');
+          toast.info('Nothing found. Please try another search value.');
         }
       } catch (error) {
         console.error('Oooopss something went wrong!!!', error);
-        setErrorMessage('Something went wrong. Please try again later.');
+        toast.error('Something went wrong. Please try again later.');
       } finally {
         loaderToggle(false);
       }
@@ -72,8 +71,8 @@ function App() {
 
   return (
     <>
+      <ToastContainer autoClose={3000} theme="colored" />
       <Searchbar onSubmit={handleSubmit} />
-      {errorMessage && <div>{errorMessage}</div>}
       {imgList.length > 0 && <ImageGallery listOfImages={imgList} />}
       {loading && <Loader />}
       {totalPage > page && inputValue && <LoadMore onClick={handleLoadMore} />}
